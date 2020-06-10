f3697208c51e484ce0b6965d5bcce85574855253
Lucas Santos <lucas.santos@nb-lsantos.grupoiv2.local>
2020-06-10 18:34:36 -0300

    organizando o codigo

--- js/script.js
+++ js/script.js
@@ -1,22 +1,12 @@
-// ativa o box de mensagem
-function showIt2() {
-  document.getElementById("msg1").style.visibility = "visible";
+// Essa função faz a ativação da caixa de texto
+function ativaMensagem() {
+  document.getElementById("caixaTexto").style.visibility = "visible";
 }
-setTimeout("showIt2()", 5000);
 
-// desativa o box de mensagem
-function hiddenIt() {
-  document.getElementById("msg1").style.visibility = "hidden";
-}
-setTimeout("hiddenIt()", 5000);
+// Depois de 5 segundos executa a função acima
+setTimeout("ativaMensagem()", 5000);
 
-// ativa novamente o box de mensagem
-function showIt3() {
-  document.getElementById("msg1").style.visibility = "visible";
-}
-setTimeout("showIt3()", 5000);
-
-// Clique para esconder o box de mensagem
-msg1.onclick = function () {
-  document.getElementById('msg1').style.visibility = "hidden";
+// Quando for dado um clique na caixa de texto a mensagem some
+caixaTexto.onclick = function () {
+  document.getElementById("caixaTexto").style.visibility = "hidden";
 };
