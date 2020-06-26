47e19780f2520b792dcee7a71216e83de59922ce
Lucas Santos <lucas.santos@grupoiv2.local>
2020-06-26 09:22:14 -0300

    refatoracao do script

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
-// Essa função faz a ativação da caixa de texto
-function ativaMensagem() {
-  document.getElementById("caixaTexto").style.visibility = "visible";
-}
-
-// Depois de 5 segundos executa a função acima
-setTimeout("ativaMensagem()", 5000);
+// Após 5 segundos passados do usuário na página será exibida a caixa de texto
+setTimeout(() => {
+  document.getElementById("caixaTexto").style.visibility = 'visible'
+}, 5000);
 
 // Quando for dado um clique na caixa de texto a mensagem some
-caixaTexto.onclick = function () {
+caixaTexto.onclick = () => {
   document.getElementById("caixaTexto").style.visibility = "hidden";
 };
